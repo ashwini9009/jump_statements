c2f64a7553d717031df5daa772ead5bfedc382b1
Ashwini Rukare <rukareashwini@gmail.com>
2026-01-06 16:24:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+package jump_statements;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		   
+		   for(int i=1;i<=10;i++)
+		   {
+			   if(i==5)
+			   {
+				   break;
+			   }
+			  System.out.println(i);
+		   }
+
+	}
+
+}
